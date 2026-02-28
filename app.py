51f6e063a0b91c8765e487dc3d6309141e9caa28
Ganesh <youremail@example.com>
2026-02-28 14:51:13 +0530

    added about page

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+@app.route("/about")
+def about():
+    return render_template("about.html")

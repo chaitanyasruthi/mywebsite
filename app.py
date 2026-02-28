9aad10c1326ed5082e012856dbe77df675eda66b
Ganesh <youremail@example.com>
2026-02-28 14:39:33 +0530

    fixed requirements

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,9 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "<h2>This is my website About page</h2>"
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
